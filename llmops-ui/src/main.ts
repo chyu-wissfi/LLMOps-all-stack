4835db0192cf9335bfca0b97acbbf03a28a1abe8
chyu-wissfi <chuanhua_yu@qq.com>
2026-03-11 17:01:00 +0800

    更新UI组件

--- llmops-ui/src/main.ts
+++ llmops-ui/src/main.ts
@@ -1,14 +1,19 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+import ArcoVue from '@arco-design/web-vue'
+import ArcoVueIcon from '@arco-design/web-vue/es/icon'
+
+import '@arco-design/web-vue/dist/arco.css'
+import '@/assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ArcoVue)
+app.use(ArcoVueIcon)
 
 app.mount('#app')
--- llmops-ui/src/views/AboutView.vue
+++ llmops-ui/src/views/AboutView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <p>Wissfi</p>
+    <a-button type="primary">注册登录</a-button>
+    <a-avatar :style="{ backgroundColor: '#3370ff' }">
+      <IconUser />
+    </a-avatar>
   </div>
 </template>
 
